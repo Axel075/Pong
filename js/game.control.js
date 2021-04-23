2433d175deca1ff1130e194c1e8243880238182b
Axel075 <48222443+Axel075@users.noreply.github.com>
2021-04-23 21:09:51 +0200

    Initial commit

---
+++ js/game.control.js
@@ -0,0 +1,28 @@
+game.control = {
+    onKeyUp : function(event) {
+        if ( event.keyCode == game.keycode.KEYDOWN ) {
+            game.playerOne.goDown = false;
+            game.playerTwo.goDown = false;
+            game.playerThree.goDown = false;
+            game.playerFour.goDown = false;
+        } else if (event.keyCode == game.keycode.KEYUP) {
+            game.playerOne.goUp = false;
+            game.playerTwo.goUp = false;
+            game.playerThree.goUp = false;
+            game.playerFour.goUp = false;
+        }
+    },
+    onKeyDown : function(event) {
+        if (event.keyCode == game.keycode.KEYDOWN) {
+            game.playerOne.goDown = true;
+            game.playerTwo.goDown = true;
+            game.playerThree.goDown = true;
+            game.playerFour.goDown = true;
+        } else if (event.keyCode == game.keycode.KEYUP) {
+            game.playerOne.goUp = true;
+            game.playerTwo.goUp = true;
+            game.playerThree.goUp = true;
+            game.playerFour.goUp = true;
+        }
+    }
+}
